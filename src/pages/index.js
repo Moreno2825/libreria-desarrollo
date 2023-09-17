77349a5262bf1628874d202a18959d32b69bc3c5
Moreno2825 <rodrigocaraveo14@gmail.com>
2023-09-17 14:12:55 -0600

    feact() create a custom component book

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from "react";
-import {ButtonStyled, Container} from "@/styles/Index.style"
+import { ButtonStyled, Container } from "@/styles/Index.style";
 import CustomButton from "@/components/CustomButton";
+
 
 export default function Home() {
   return (
     <Container>
       <h1>Hola</h1>
       <ButtonStyled>Botón de mui</ButtonStyled>
-      <CustomButton buttonText="Componente de boton"/>
+      <CustomButton buttonText="Componente de boton" />
     </Container>
-  )
+  );
 }
--- src/styles/Index.style.js
+++ src/styles/Index.style.js
@@ -2,9 +2,8 @@ import styled from "@emotion/styled";
 import { Button } from "@mui/material";
 
 export const Container = styled.div`
-  width: 100vw;
+  width: 100%;
   height: 95vh;
-  background-color: aliceblue;
   display: flex;
   align-items: center;
   justify-content: center;
