680770069b35f44c4d6245375bcafe3c7868cfc8
Mariana Trujillo Islas <marianatrujillo27@hotmail.com>
2023-09-29 10:47:12 -0600

    fix(): new style

--- src/components/CustomButton/index.js
+++ src/components/CustomButton/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { ButtonStyled } from "./index.style";
 
-const CustomButton = ({ fullWidth, buttonText, onClick, type, disable}) => {
+const CustomButton = ({ fullWidth, buttonText, onClick, type, disable, style}) => {
+    const customStyle = {
+        borderRadius: "10px",
+        background: "#FFF",
+        color: "#7d6e83",
+        border: "2px solid #7d6e83"
+    }
+
     return (
-        <ButtonStyled fullWidth={fullWidth} onClick={onClick} type={type} disabled={disable}>
-            {buttonText}
+        <ButtonStyled fullWidth={fullWidth} onClick={onClick} type={type} disabled={disable} style={customStyle}>
+            {buttonText} 
         </ButtonStyled>
     );
 };
