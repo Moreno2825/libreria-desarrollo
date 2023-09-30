e9d2962747cd6bc04c3437d919d382fc87d41848
Eduardo Martin <lalom1227@gmail.com>
2023-09-29 20:57:20 -0600

    fix(): movimiento

--- src/components/CustomIndividualBook/index.style.js
+++ src/components/CustomIndividualBook/index.style.js
@@ -77,3 +77,24 @@ export const ContainerButtons = styled.div`
   gap: 32px;
   padding: 24px 0px;
 `;
+
+export const ImagenD = styled.div`
+    display: flex;     
+    justify-content: center;
+    align-items: center;
+`;
+
+export const Buttons = styled.div`
+    display: flex;     
+    justify-content: center;
+    margin-top: 30px;
+
+    & > button {
+        min-width: 225px; 
+        flex-basis: 0;
+        margin-right: 18px; 
+    }
+    & > button:last-child {
+        margin-right: 0; 
+    }
+`;
