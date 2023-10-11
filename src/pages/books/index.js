993c00e916b8d23a8ee54bfd047cf6ae47c2d16e
Moreno2825 <rodrigocaraveo14@gmail.com>
2023-10-11 09:01:59 -0600

    refactor();

--- src/pages/books/index.js
+++ src/pages/books/index.js
@@ -100,7 +100,7 @@ export default function ViewBook() {
         />
       </AddContainer>
       <div>
-        <RecentBooks>Añadidos recientemente</RecentBooks>
+        <RecentBooks>Todos</RecentBooks>
         <BooksContainer>
           {books.map((book) => (
             <Link key={book._id} href={`/books/view/${book._id}`}>
