463e039a464b26b32f28f3a273e524c8edf06ef1
Eduardo Martin <lalom1227@gmail.com>
2023-10-19 11:48:11 -0600

    feat():HomeUser

--- src/pages/homeUser.js
+++ src/pages/homeUser.js
@@ -1,82 +1,56 @@
-import GetAllBookUseCase from "@/application/usecases/bookUseCase/GetAllBookUseCase";
-import GetAllCategoryUseCase from "@/application/usecases/categoryUseCase/GetAllCategoryUseCase";
-import CustomButton from "@/components/CustomButton";
-import CustomInput from "@/components/CustomInput";
-import CustomModal from "@/components/CustomModal";
-import CustomTextArea from "@/components/CustomTextArea";
-import BookRepo from "@/infraestructure/implementation/httpRequest/axios/BookRepo";
-import CategoryRepo from "@/infraestructure/implementation/httpRequest/axios/CategoryRepo";
+import { ContainerUs, Down, Img, Inf, InfIcon, InfText, InfTitles, Line, Phrase, Text, TextWhy, Why } from "@/styles/HomeUsers";
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import Image from "next/image";
+import CustomButton from "@/components/CustomButton";
+import { faBookOpenReader, faCircleInfo } from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
-  const [isOpenForgotPassword, setOpenForgotPassword] = useState(false);
-  const [categories, setCategories] = useState([]);
-
-  const toggleForgotPasswordModal = () =>
-    setOpenForgotPassword((isOpenForgotPassword) => !isOpenForgotPassword);
-
-  const {
-    handleSubmit,
-    control,
-    formState: { errors },
-  } = useForm({});
-
-  const categoryRepo = new CategoryRepo();
-  const getAllCategoryUseCase = new GetAllCategoryUseCase(categoryRepo);
-
-  const fetchCategories = async () => {
-    try {
-      const response = await getAllCategoryUseCase.run();
-      const fetchedCategories = response.data;  // Paso 2: Extraer categorías de la propiedad data
-      setCategories(fetchedCategories);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchCategories();
-    fetchBook();
-  }, []);
-
-  const bookRepo = new BookRepo();
-  const getAllBookUseCase = new GetAllBookUseCase(bookRepo);
-  
-  const fetchBook = async () => {
-    try {
-      const response = await getAllBookUseCase.run();
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-    }
-  }
 
   return (
     <div>
-      {/* <ul>
-        {categories.map(category => (
-          <li key={category._id}>{category.name}</li>
-        ))}
-      </ul> */}
-      {/* <CustomButton
-        buttonText="Agrega un nuevo libro"
-        onClick={toggleForgotPasswordModal}
+     <ContainerUs>
+      <Phrase>
+      Un mundo de historias en tu pantalla.
+      </Phrase>
+      <Line></Line>
+      <Text>
+      Bienvenido a eBookCloud, tu destino literario virtual. 
+      En nuestra plataforma, te ofrecemos acceso a una amplia y diversa 
+      selección de libros de todos los géneros. Navega por nuestra biblioteca 
+      digital, donde cada página es una puerta a un mundo nuevo, una aventura por descubrir.
+      </Text>
+      <CustomButton
+        buttonText="Comenzar a descubrir"
       />
-      <CustomModal
-        open={isOpenForgotPassword}
-        onClose={toggleForgotPasswordModal}
-        title="Agregar"
-        message="Añade un nuevo libro ingresando los datos correspondientes."
-      >
-        <CustomInput label="Título" name="title" control={control} />
-        <CustomInput label="Autor" name="autor" control={control} />
-        <CustomTextArea label="Sipnosis" sipnosis="hshshsh" />
-        <div style={{ display: "flex", gap: "26px" }}>
-          <CustomInput label="Sipnosis" name="sipnosis" control={control} />
-          <CustomInput label="Sipnosis" name="sipnosis" control={control} />
-        </div>
-      </CustomModal> */}
+     </ContainerUs>
+     <Img>
+     <Image src="/img/PhotoRoom.png" width={434} height={390} alt="logo"/>
+     </Img>
+
+    <Down>
+     <Why>
+     ¿Por qué deberías elegirnos?
+     <TextWhy>Descubre nuevas historias, autores innovadores 
+      y expande tus horizontes literarios con nosotros.</TextWhy>
+     </Why>
+     <Inf>
+     <InfIcon icon={faBookOpenReader} />
+     <InfTitles>Variedad de títulos</InfTitles>
+     <InfText>
+     Ofrecemos una inmensa variedad de títulos que abarcan desde clásicos
+      literarios hasta las últimas novedades en todos los géneros imaginables.
+     </InfText>
+     </Inf>
+     <Inf>
+      <InfIcon icon={faCircleInfo} />
+      <InfTitles>Información detallada</InfTitles>
+      <InfText>
+      Proporcionamos información sobre cada libro, lo que te permite tomar decisiones 
+      informadas sobre tus compras y explorar cada libro antes de adquirirlo.
+      </InfText>
+     </Inf>
+     </Down>
     </div>
   );
 };
--- src/styles/HomeUsers.js
+++ src/styles/HomeUsers.js
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
-import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const ContainerUs = styled.div`
 display: inline-flex;
@@ -10,8 +10,6 @@ gap: 26px;
 flex-shrink: 0;
 display: inline-block;
 margin-left: 120px;
-
-display: inline-block;
 vertical-align: top;
 margin-top: 120px;
 `;
@@ -44,7 +42,7 @@ margin-top: 85px;
 
 export const Line = styled.div`
 width: 430px;
-  width: 300px; 
+height: .5px;
   background-color: black;
   margin-top: 26px;
   margin-bottom: 26px;
@@ -52,6 +50,75 @@ width: 430px;
 
 export const Why = styled.div`
   width: 300px; 
-  width: 150px; 
-
+  height: 150px; 
+  color: #2A2A2A;
+  font-family: Poppins;
+  font-size: 18px;
+  font-weight: 600;
+  margin-left: 120px;
+  margin-top: 146px;
+  display: inline-block;
+  vertical-align: top;
+  margin-right: 55px;
 `;
+
+export const TextWhy = styled.div`
+  width: 300px; 
+  color: #414141;
+  font-family: Poppins;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  margin-top: 16px;
+  
+`;
+
+export const Inf = styled.div`
+  width: 230px;
+  height: 230px;
+  flex-shrink: 0; 
+  background: #DAD6DC;
+  display: inline-block;
+  vertical-align: top;
+  margin-right: 75px;
+  margin-top: 111px;
+`;
+
+export const Down = styled.div`
+`;
+
+export const InfIcon = styled(FontAwesomeIcon)`
+  width: 30px;
+  height: 30px;
+  flex-shrink: 0; 
+  margin-left: 18px;
+  margin-top: 27px;
+`;
+
+export const InfTitles = styled.div`
+width: 197px;
+color: #2A2A2A;
+font-family: Poppins;
+font-size: 16px;
+font-style: normal;
+font-weight: 700;
+line-height: normal;
+margin-left: 15px;
+  margin-top: 17px;
+`;
+
+export const InfText = styled.div`
+width: 197px;
+color: #414141;
+font-family: Poppins;
+font-size: 12px;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
+margin-left: 15px;
+  margin-top: 16px;
+`;
+
+
+
