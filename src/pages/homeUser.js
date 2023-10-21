1a86a66192aeb3ff75fde91ce437e40bba3a5143
Mariana Trujillo Islas <marianatrujillo27@hotmail.com>
2023-10-21 12:48:13 -0600

    style(): adapting page

--- src/pages/homeUser.js
+++ src/pages/homeUser.js
@@ -1,56 +1,76 @@
-import { ContainerUs, Down, Img, Inf, InfIcon, InfText, InfTitles, Line, Phrase, Text, TextWhy, Why } from "@/styles/HomeUsers";
+import {
+  Container,
+  ContainerUs,
+  Down,
+  Img,
+  Inf,
+  InfIcon,
+  InfText,
+  InfTitles,
+  Line,
+  Phrase,
+  Text,
+  TextWhy,
+  Why,
+} from "@/styles/HomeUsers";
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import Image from "next/image";
 import CustomButton from "@/components/CustomButton";
-import { faBookOpenReader, faCircleInfo } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBookOpenReader,
+  faCircleInfo,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
-
   return (
     <div>
-     <ContainerUs>
-      <Phrase>
-      Un mundo de historias en tu pantalla.
-      </Phrase>
-      <Line></Line>
-      <Text>
-      Bienvenido a eBookCloud, tu destino literario virtual. 
-      En nuestra plataforma, te ofrecemos acceso a una amplia y diversa 
-      selección de libros de todos los géneros. Navega por nuestra biblioteca 
-      digital, donde cada página es una puerta a un mundo nuevo, una aventura por descubrir.
-      </Text>
-      <CustomButton
-        buttonText="Comenzar a descubrir"
-      />
-     </ContainerUs>
-     <Img>
-     <Image src="/img/PhotoRoom.png" width={434} height={390} alt="logo"/>
-     </Img>
-
-    <Down>
-     <Why>
-     ¿Por qué deberías elegirnos?
-     <TextWhy>Descubre nuevas historias, autores innovadores 
-      y expande tus horizontes literarios con nosotros.</TextWhy>
-     </Why>
-     <Inf>
-     <InfIcon icon={faBookOpenReader} />
-     <InfTitles>Variedad de títulos</InfTitles>
-     <InfText>
-     Ofrecemos una inmensa variedad de títulos que abarcan desde clásicos
-      literarios hasta las últimas novedades en todos los géneros imaginables.
-     </InfText>
-     </Inf>
-     <Inf>
-      <InfIcon icon={faCircleInfo} />
-      <InfTitles>Información detallada</InfTitles>
-      <InfText>
-      Proporcionamos información sobre cada libro, lo que te permite tomar decisiones 
-      informadas sobre tus compras y explorar cada libro antes de adquirirlo.
-      </InfText>
-     </Inf>
-     </Down>
+      <Container>
+        <ContainerUs>
+          <Phrase>Un mundo de historias en tu pantalla.</Phrase>
+          <Line></Line>
+          <Text>
+            Bienvenido a eBookCloud, tu destino literario virtual. En nuestra
+            plataforma, te ofrecemos acceso a una amplia y diversa selección de
+            libros de todos los géneros. Navega por nuestra biblioteca digital,
+            donde cada página es una puerta a un mundo nuevo, una aventura por
+            descubrir.
+          </Text>
+          <CustomButton buttonText="Comenzar a descubrir" />
+        </ContainerUs>
+        <div>
+          <Image src="/img/PhotoRoom.png" width={300} height={270} alt="logo" />
+        </div>
+      </Container>
+      <div style={{display: "flex", alignItems: "center", justifyContent: "center", gap: "180px"}}>
+        <Why>
+          ¿Por qué deberías elegirnos?
+          <TextWhy>
+            Descubre nuevas historias, autores innovadores y expande tus
+            horizontes literarios con nosotros.
+          </TextWhy>
+        </Why>
+        <div style={{ display: "flex", gap: "62px" }}>
+          <Inf>
+            <InfIcon icon={faBookOpenReader} />
+            <InfTitles>Variedad de títulos</InfTitles>
+            <InfText>
+              Ofrecemos una inmensa variedad de títulos que abarcan desde
+              clásicos literarios hasta las últimas novedades en todos los
+              géneros imaginables.
+            </InfText>
+          </Inf>
+          <Inf>
+            <InfIcon icon={faCircleInfo} />
+            <InfTitles>Información detallada</InfTitles>
+            <InfText>
+              Proporcionamos información sobre cada libro, lo que te permite
+              tomar decisiones informadas sobre tus compras y explorar cada
+              libro antes de adquirirlo.
+            </InfText>
+          </Inf>
+        </div>
+      </div>
     </div>
   );
 };
--- src/styles/HomeUsers.js
+++ src/styles/HomeUsers.js
@@ -1,124 +1,103 @@
 import styled from "@emotion/styled";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 120px;
+`;
+
 export const ContainerUs = styled.div`
-display: inline-flex;
-height: 351px;
-flex-direction: column;
-align-items: flex-start;
-gap: 26px;
-flex-shrink: 0;
-display: inline-block;
-margin-left: 120px;
-vertical-align: top;
-margin-top: 120px;
+  height: 351px;
+  flex-direction: column;
+  align-items: flex-start;
+  flex-shrink: 0;
+  display: inline-block;
+  vertical-align: top;
 `;
 
 export const Phrase = styled.div`
-width: 437px;
-color: #2A2A2A;
-font-family: Libre Baskerville;
-font-size: 36px;
-font-style: normal;
-font-weight: 400;
-line-height: normal;
+  width: 437px;
+  color: #2a2a2a;
+  font-family: Libre Baskerville;
+  font-size: 26px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
 `;
 
 export const Text = styled.div`
-width: 532px;
-color: #414141;
-font-family: Poppins;
-font-size: 18px;
-font-style: normal;
-font-weight: 500;
-line-height: normal;
-`;
-
-export const Img = styled.div`
-display: inline-block;
-vertical-align: top;
-margin-top: 85px;
+  width: 532px;
+  color: #414141;
+  font-family: Poppins;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
 `;
 
 export const Line = styled.div`
-width: 430px;
-height: .5px;
-  background-color: black;
-  margin-top: 26px;
-  margin-bottom: 26px;
+  width: 430px;
+  height: 1px;
+  background-color: #2A2A2A;
+  margin-top: 24px;
+  margin-bottom: 24px;
 `;
 
 export const Why = styled.div`
-  width: 300px; 
-  height: 150px; 
-  color: #2A2A2A;
+  width: 300px;
+  height: 150px;
+  color: #2a2a2a;
   font-family: Poppins;
   font-size: 18px;
   font-weight: 600;
-  margin-left: 120px;
-  margin-top: 146px;
-  display: inline-block;
-  vertical-align: top;
-  margin-right: 55px;
 `;
 
 export const TextWhy = styled.div`
-  width: 300px; 
+  width: 300px;
   color: #414141;
   font-family: Poppins;
-  font-size: 18px;
+  font-size: 16px;
   font-style: normal;
   font-weight: 400;
   line-height: normal;
   margin-top: 16px;
-  
 `;
 
 export const Inf = styled.div`
-  width: 230px;
-  height: 230px;
-  flex-shrink: 0; 
-  background: #DAD6DC;
-  display: inline-block;
-  vertical-align: top;
-  margin-right: 75px;
-  margin-top: 111px;
-`;
-
-export const Down = styled.div`
+  width: 200px;
+  height: 200px;
+  background: #dad6dc;
+  display: flex;
+  flex-direction: column;
 `;
 
 export const InfIcon = styled(FontAwesomeIcon)`
-  width: 30px;
-  height: 30px;
-  flex-shrink: 0; 
+  width: 24px;
+  height: 24px;
   margin-left: 18px;
-  margin-top: 27px;
+  margin-top: 20px;
 `;
 
 export const InfTitles = styled.div`
-width: 197px;
-color: #2A2A2A;
-font-family: Poppins;
-font-size: 16px;
-font-style: normal;
-font-weight: 700;
-line-height: normal;
-margin-left: 15px;
-  margin-top: 17px;
+  width: 197px;
+  color: #2a2a2a;
+  font-family: Poppins;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  margin: 16px 18px 10px;
 `;
 
 export const InfText = styled.div`
-width: 197px;
-color: #414141;
-font-family: Poppins;
-font-size: 12px;
-font-style: normal;
-font-weight: 400;
-line-height: normal;
-margin-left: 15px;
-  margin-top: 16px;
+  width: 170px;
+  color: #414141;
+  font-family: Poppins;
+  font-size: 11px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  margin: 0px 18px;
 `;
-
-
-
