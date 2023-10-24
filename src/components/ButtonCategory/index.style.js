e03e71059eed57f8f7b8a313dad1090989653940
Mariana Trujillo Islas <marianatrujillo27@hotmail.com>
2023-10-24 17:38:08 -0600

    feat(): api consumer

--- src/components/ButtonCategory/index.style.js
+++ src/components/ButtonCategory/index.style.js
@@ -2,8 +2,8 @@ import styled from "@emotion/styled";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const ButtonStyled = styled.button`
-  width: 100px;
-  height: 100px;
+  width: 95px;
+  height: 95px;
   border-radius: 20px;
   background: #f1f1f1;
   display: flex;
@@ -15,7 +15,7 @@ export const ButtonStyled = styled.button`
   gap: 16px;
   color: #2a2a2a;
   font-family: Poppins;
-  font-size: 16px;
+  font-size: 15px;
   font-weight: 500;
   &:active {
     scale: 0.98;
@@ -24,6 +24,6 @@ export const ButtonStyled = styled.button`
 
 export const Icon = styled(FontAwesomeIcon)`
   color: #2a2a2a;
-  width: 32px;
-  height: 32px;
+  width: 24px;
+  height: 24px;
 `;
